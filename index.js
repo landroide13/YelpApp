4c08c953cae1e7d4b9dea2a9b5a40662286911f5
Luis Andrade <landroide@Luiss-MacBook-Pro.local>
2022-12-07 11:05:16 +1300

    CRUD Camp Done

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const CampSchema = require('./models/campground');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(method('_method'));
+
 
 //Uses
 app.use(express.static(path.join(__dirname, '/public')));
@@ -29,13 +32,39 @@ app.get('/campgrounds', async(req, res) =>{
     res.render('campgrounds/index', { camps });
 })
 
+app.get('/campgrounds/new', (req, res) =>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req, res) =>{
+    const campground = new CampSchema(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async(req, res) =>{
     const { id } = req.params;
     const camp = await CampSchema.findById(id)
     res.render('campgrounds/show', { camp });
 })
 
+app.get('/campgrounds/:id/edit', async(req, res) =>{
+    const { id } = req.params;
+    const camp = await CampSchema.findById(id);
+    res.render('campgrounds/edit', { camp });
+})
 
+app.put('/campgrounds/:id', async(req, res) =>{
+    const { id } = req.params;
+    const updated = await CampSchema.findByIdAndUpdate(id, {...req.body.campground}, { new: true });
+    res.redirect(`/campgrounds/${updated._id}`)
+})
+
+app.delete('/campgrounds/:id', async(req, res) =>{
+    const { id } = req.params;
+    await CampSchema.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 
 
